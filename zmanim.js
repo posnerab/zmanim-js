fddc760da1939563498a1d642aed4df65c5d153b
Xander Posner <me@xan.cloud>
2024-06-05 10:26:37 -0500

    fix rounding

--- zmanim.js
+++ zmanim.js
@@ -111,7 +111,7 @@ function scheduleCronTrigger(triggerTime, zmanim, label, offset, isReminder = fa
 
         if (isReminder) {
             const zmanTime = DateTime.fromISO(zmanim.times[label]).setZone('America/Chicago');
-            const timeDiff = now.diff(zmanTime, 'minutes').minutes;
+            const timeDiff = Math.round(now.diff(zmanTime, 'minutes').minutes.minutes);
             const humanReadable = `${zmanimDefinitions[label]} is ${formatTimeDifference(timeDiff)} away (${zmanTime.toFormat('h:mm a')})`;
             const payload = { value1: humanReadable };
             await triggerIFTTT(payload);
